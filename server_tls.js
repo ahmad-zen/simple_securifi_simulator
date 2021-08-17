4568d75963b28c15fd94ef6b404fcf1f7d28dd48
Ahmad Alhayek <Ahmad.Alhayek@zenecosystems.com>
2021-08-17 00:35:44 -0700

    init simple securifi sim git

---
+++ server_tls.js
@@ -0,0 +1,125 @@
+const fs = require("fs");
+const express = require("express");
+
+class SecurifiServerSimulator {
+    securifiServer;
+    connectedSockets = [];
+
+    constructor(securifiPort){
+        const socketServer = require("tls").createServer({
+            cert: fs.readFileSync('./cert/securifi-server-cert.pem'),
+            key: fs.readFileSync('./cert/securifi-server-key.pem'),
+            ca: [fs.readFileSync('./cert/securifi-server-cert.pem')],
+            requestCert: false,
+            rejectUnauthorized: false
+        }, (socket) => {
+            this.connectedSockets.push(socket);
+            console.log('server connected', socket.authorized ? 'authorized' : 'unauthorized');
+            // socket.write(JSON.stringify({foo : 'test', msg : 'did you get it ?'}) + "\n");
+            this.ConfigureListeners(socket);
+        });
+
+        this.securifiServer = socketServer;
+        this.securifiServer.listen(securifiPort);
+        console.log('started socket server');      
+    }
+
+    ConfigureListeners(socket){
+        socket.on('end', function(end) {
+            console.log('EOT (End Of Transmission)');
+            console.log(socket.readyState);
+            this.connectedSockets.map(s => {
+                s.destroy();
+            });
+        });
+
+        socket.on('error', function(err){
+            this.connectedSockets.map(s => {
+                s.destroy();
+            });
+            console.error(err);
+        });
+    }
+
+    EmitData(data){
+        if(this.connectedSockets.length !== 0){
+            this.connectedSockets.map(s => {
+                s.write(`securifi_data_received: ${data}`)
+            });
+            console.log("emitted data");
+        }
+    }
+
+    EmitAlmondList(almondList){
+        if(this.connectedSockets.length !== 0){
+            this.connectedSockets.map(s => {
+                s.write(`almond_list_received: ${almondList}`);
+            });
+            console.log("emitted almond list");
+        }
+    }
+
+    EmitDeviceList(deviceList){
+        if(this.connectedSockets.length !== 0){
+            this.connectedSockets.map(s => {
+                s.write(`device_list_received: ${deviceList}`);
+            });
+            console.log("emitted device list");
+        }
+    }
+}
+
+class SecurifiServerController {
+    httpServer;
+    securifiServer;
+
+    constructor(apiPort, securifiServer){
+        this.httpServer = express();
+        this.httpServer.listen(apiPort);
+        console.log("started controller");
+
+        this.securifiServer = securifiServer;
+    }
+
+    SetupRoutes(){
+        this.httpServer.use(express.json());
+
+        this.httpServer.post('/DeviceData', (req,res) => {
+            this.SendDeviceData(req.body);
+            res.send(req.body);
+        });
+
+        this.httpServer.post('/AlmondList', (req,res) => {
+            this.SendAlmondList(req.body);
+            res.send(req.body);
+        });
+
+        this.httpServer.post('/DeviceList', (req,res) => {
+            this.SendDeviceList(req.body);
+            res.send(req.body);
+        });
+    }
+
+    SendDeviceData(data){
+        data = JSON.stringify(data);
+        console.log(`Sending device data ${data}`);
+        this.securifiServer.EmitData(data);
+    }
+
+    SendAlmondList(almondList){
+        almondList = JSON.stringify(almondList);
+        console.log(`Sending Almond List ${almondList}`);
+        this.securifiServer.EmitAlmondList(almondList);
+    }
+
+    SendDeviceList(deviceList){
+        deviceList = JSON.stringify(deviceList);
+        console.log(`Sent Device List ${deviceList}`);
+        this.securifiServer.EmitDeviceList(deviceList);
+    }
+}
+
+
+let securifiServer = new simulator.SecurifiServerSimulator(1028);
+let controller = new simulator.SecurifiServerController(5000, securifiServer);
+controller.SetupRoutes();
