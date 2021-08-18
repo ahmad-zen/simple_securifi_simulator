6b1d24ec56aa35007fc298464543d1d11e7467b4
Ahmad Alhayek <Ahmad.Alhayek@zenecosystems.com>
2021-08-18 16:47:00 -0700

    fixed keep alive and handling list of commands instead of just a single command

--- server_tls.js
+++ server_tls.js
@@ -24,7 +24,7 @@ class SecurifiServerSimulator {
 
     ConfigureListeners(socket){
         var keepAliveInterval = setInterval(() => {
-            socket.write({"root":{"KeepAlive":"KEEP_ALIVE"}});
+            socket.write(JSON.stringify({"root":{"KeepAlive":"KEEP_ALIVE"}}));
         }, 60000);
 
         socket.on('end', function(end) {
@@ -33,57 +33,67 @@ class SecurifiServerSimulator {
             socket.destroy();
         });
 
+        socket.on('error', function(err){
+            clearInterval(keepAliveInterval);
+            socket.destroy();
+            console.error(err);
+        });
+
         socket.on('data', function(data){
             console.log(`command received by securifi simulator: ${data}`);
             //{"MobileInternalIndex":34,"CommandType":"UpdateDeviceIndex","AlmondMAC":"251176216363884","ID":"10","Index":10,"Value":"0a 0b 01 00"}
             try {
                 let dataAsString = data+"";
-                let startOfObject = dataAsString.indexOf('{');
-                let endOfObject = dataAsString.indexOf('}') + 1;
-                let jsonString = dataAsString.substring(startOfObject, endOfObject);
-                console.log(`JSON as string: ${jsonString}`);
+                let splitCommands = dataAsString.split('{');
 
-                let command = JSON.parse(jsonString);
-                if(command.CommandType = 'UpdateDeviceIndex' && command.Value.substring(command.Value.length - 5).startsWith('01'))
-                {
-                    if(command.Index == NaN){
-                        console.log("no index, cannot respond");
-                        return;
+                for(let i = 0; i < splitCommands.length; i++){
+                    if(splitCommands[i] == ""){
+                      continue;
                     }
-                    //{"CommandType":"DynamicIndexUpdated","Action":"UpdateIndex","HashNow":"9dca5eee5590afb2ad5736c38490e8b3","Devices":{"10":{"DeviceValues":{"10":{"Name":"CUSTOM_MESSAGE","Value":"01 0a 0b 0d 01 00 03 0b 2e 02 01 02 20 00","Type":"92","EndPoint":"1","CommandClassID":"-1","CommandIndex":"-1"}}}},"AlmondMAC":"251176216363884","time":"1629243631.59693"}
-                    let response = JSON.stringify({
-                        "CommandType":"DynamicIndexUpdated",
-                        "Action":"UpdateIndex",
-                        "HashNow":"9dca5eee5590afb2ad5736c38490e8b3",
-                        "Devices":{
-                            [command.Index] : {
-                                "DeviceValues":{
-                                    [command.Index]:{
-                                        "Name":"CUSTOM_MESSAGE",
-                                        "Value": this.firmwareVersion,
-                                        "Type":"92",
-                                        "EndPoint":"1",
-                                        "CommandClassID":"-1",
-                                        "CommandIndex":"-1"
+                  
+                    let command = "{" + splitCommands[i];
+                    let startOfObject = command.indexOf('{');
+                    let endOfObject = command.indexOf('}') + 1;
+                    let jsonString = command.substring(startOfObject, endOfObject);
+                    console.log(`JSON as string: ${jsonString}`);
+
+                    let command = JSON.parse(jsonString);
+                    if(command.CommandType == 'UpdateDeviceIndex' && command.Value.substring(command.Value.length - 5).startsWith('01'))
+                    {
+                        if(command.Index == NaN){
+                            console.log("no index, cannot respond");
+                            return;
+                        }
+                        //{"CommandType":"DynamicIndexUpdated","Action":"UpdateIndex","HashNow":"9dca5eee5590afb2ad5736c38490e8b3","Devices":{"10":{"DeviceValues":{"10":{"Name":"CUSTOM_MESSAGE","Value":"01 0a 0b 0d 01 00 03 0b 2e 02 01 02 20 00","Type":"92","EndPoint":"1","CommandClassID":"-1","CommandIndex":"-1"}}}},"AlmondMAC":"251176216363884","time":"1629243631.59693"}
+                        let response = JSON.stringify({
+                            "CommandType":"DynamicIndexUpdated",
+                            "Action":"UpdateIndex",
+                            "HashNow":"9dca5eee5590afb2ad5736c38490e8b3",
+                            "Devices":{
+                                [command.Index] : {
+                                    "DeviceValues":{
+                                        [command.Index]:{
+                                            "Name":"CUSTOM_MESSAGE",
+                                            "Value": this.firmwareVersion,
+                                            "Type":"92",
+                                            "EndPoint":"1",
+                                            "CommandClassID":"-1",
+                                            "CommandIndex":"-1"
+                                        }
                                     }
                                 }
-                            }
-                        },
-                        "AlmondMAC":"251176216363884",
-                        "time":"1629243631.59693"
-                    });
-                    socket.write(response);
-                    console.log(`response sent to securifi worker ${response}`);
+                            },
+                            "AlmondMAC": command.AlmondMAC,
+                            "time":"1629243631.59693"
+                        });
+                        socket.write(response);
+                        console.log(`response sent to securifi worker ${response}`);
+                    }
                 }
             } catch (error) {
                 console.log("could not parse request");
                 console.log(error);
             }
-        });
-
-        socket.on('error', function(err){
-            socket.destroy();
-            console.error(err);
         });
     }
 
